9714a94f8c576d1fa981a7eee2a8948f1df39eb3
lijie0981 <974277025@qq.com>
2019-10-22 09:31:05 +0800

    add cache

--- src/http-push.ts
+++ src/http-push.ts
@@ -4,16 +4,20 @@
  * @Last Modified by: qiansc
  * @Last Modified time: 2019-05-09 19:13:40
  */
-
+import * as crypto from 'crypto';
+import * as fs from 'fs';
 import {sync} from 'glob';
 import { File } from 'gulp-util';
-import {Transform} from './cacheTransform';
+import { dirname, resolve } from 'path';
+import stream = require('readable-stream');
 import { include } from './filter';
 import { Restrictor } from './restrictor';
-
+const Transform = stream.Transform;
 export function httpPush(options: IDeployOption[]) {
   const restrictor = new Restrictor();
-  const fileOptions: fileOption = {};
+  const fileOptions: FileOption = {};
+  const cachePath = resolve(process.cwd(), options[0].cachePath ? options[0].cachePath : '.http-push-cache/');
+  // removeCache(cachePath);
   options.forEach((item) => {
     if (item.match) {
       sync(item.match, {realpath: true}).forEach((file) => {
@@ -27,21 +31,38 @@ export function httpPush(options: IDeployOption[]) {
     objectMode: true,
     transform: (file: File, enc, callback) => {
       if (!file.isDirectory()) {
-        const option = fileOptions[file.path];
-        restrictor.add({
-          host: option.host,
-          retry: 2,
-          to: option.to,
-
-        }, {
-          contents: file.contents,
-          relative: '/' + file.relative,
-        });
-      }
+        if (file && !fs.existsSync(cachePath + getMd5(file.path + file.stat.mtimeMs))) {
+            const option = fileOptions[file.path];
+            restrictor.add({
+              host: option.host,
+              retry: 2,
+              to: option.to}, {
+              contents: file.contents,
+              relative: '/' + file.relative,
+            });
+            fs.writeFileSync(cachePath + getMd5(file.path + file.stat.mtimeMs), 'info');
+          }
+        }
       callback();
     },
   });
 }
+function getMd5(str: string) {
+    const md5sum = crypto.createHash('md5');
+    md5sum.update(str, 'utf8');
+    return md5sum.digest('hex').substring(0, 32);
+}
+// function removeCache(cachePath) {
+//   const toDay = +new Date();
+//   sync(cachePath + '*').forEach((item) => {
+//     console.log(item);
+//     fs.stat(item, (err, stats) => {
+//         if (stats.ctimeMs < toDay) {
+//           fs.unlinkSync(item);
+//         }
+//     });
+//   });
+// }
 
 interface IDeployOption {
   /** 要部署的机器HOST */
@@ -50,7 +71,9 @@ interface IDeployOption {
   match?: string;
   /** 要部署的basePath */
   to: string;
+  /** 缓存存放地址 */
+  cachePath?: string;
 }
-interface fileOption {
-  [propName: string]: IDeployOption
+interface FileOption {
+  [propName: string]: IDeployOption;
 }
